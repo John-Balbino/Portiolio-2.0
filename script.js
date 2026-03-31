77dab59b7f4243cd22f2960b6b943859efd18ab7
JohnBalbino <john.balbino01@gmail.com>
2026-03-31 18:22:09 -0300

    correção de dados no script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function whats(){
-    const number = '11959768301'
+    const nEncode = 'MTE5NTk3NjgzMDE='
+    const number = atob(nEncode)
     const link ='https:wa.me/' + number
     window.open(link)
 }
