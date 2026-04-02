b4b78ec253057ce62a7b9a14a47bc4655901a8b2
JohnBalbino <john.balbino01@gmail.com>
2026-04-01 21:29:58 -0300

    ajuste no script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function whats(){
     const nEncode = 'MTE5NTk3NjgzMDE='
     const number = atob(nEncode)
-    const link ='https:wa.me/' + number
+    const link ='https://wa.me/' + number
     window.open(link)
 }
