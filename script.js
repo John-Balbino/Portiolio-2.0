1175df4f9d7fb049d4f50811ae79c4221039daaa
JohnBalbino <john.balbino01@gmail.com>
2026-04-07 11:02:25 -0300

    adicionado função de slide nos projetos

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function whats(){
     const link ='https://wa.me/' + number
     window.open(link)
 }
+
+//fade para os projetos em jquery
+$('.box-cards').hide();
+
+$('#btn-projetos').click(function(){
+    $('.box-cards').fadeToggle(1000);
+});
